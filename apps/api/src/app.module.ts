c8e41da0bf86fa0127e9f5c8711d51f305fb2ae8
Joao Roberto <joaoroberto4584@gmail.com>
2025-02-18 01:51:39 -0300

    feat: add middleware jwt

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import {
   Module,
+  RequestMethod,
   type MiddlewareConsumer,
   type NestModule,
 } from '@nestjs/common';
 import { PrismaModule } from 'infrastructure/database/prisma.module';
+import { JwtAuthMiddleware } from 'modules/shared/middleware/jwt-auth.middleware';
 import { LoggingMiddleware } from 'modules/shared/middleware/logging.middleware';
 import { UserModule } from 'modules/user/user.module';
 
@@ -13,5 +15,10 @@ import { UserModule } from 'modules/user/user.module';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggingMiddleware).forRoutes('*');
+
+    consumer
+      .apply(JwtAuthMiddleware)
+      .exclude({ path: 'user/integration', method: RequestMethod.POST })
+      .forRoutes('*');
   }
 }
