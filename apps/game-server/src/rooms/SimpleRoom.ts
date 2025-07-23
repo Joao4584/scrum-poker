a6a742893ed93a2f39190e446eb4829b23ad08ce
Joao Roberto <joaoroberto4584@gmail.com>
2025-07-23 00:47:23 -0300

    feat: game-server: init

---
+++ apps/game-server/src/rooms/SimpleRoom.ts
@@ -0,0 +1,31 @@
+import { Room, Client } from "@colyseus/core";
+import { Player, State } from "./schema/SimpleRoomState";
+
+export class SimpleRoom extends Room<State> {
+  onCreate(options: any) {
+    this.setState(new State());
+
+    this.onMessage("move", (client, input) => {
+      const player = this.state.players.get(client.sessionId);
+      player.x = input.x;
+      player.y = input.y;
+    });
+  }
+
+  onJoin(client: Client, options: any) {
+    console.log(client.sessionId, "joined!");
+    const player = new Player();
+    player.x = Math.random() * 800;
+    player.y = Math.random() * 600;
+    this.state.players.set(client.sessionId, player);
+  }
+
+  onLeave(client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!");
+    this.state.players.delete(client.sessionId);
+  }
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...");
+  }
+}
